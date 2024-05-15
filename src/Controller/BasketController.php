73af72b4b46d5bc85013f4576a19724bb7df1af7
nikolozMamyan <nikoloz.mamyan@gmail.com>
2024-05-15 18:05:20 +0200

    hotfix

--- src/Controller/BasketController.php
+++ src/Controller/BasketController.php
@@ -2,17 +2,50 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\BasketRepository;
+use App\Repository\ProductRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class BasketController extends AbstractController
 {
     #[Route('/basket', name: 'app_basket')]
-    public function index(): Response
+    public function showBasket(EntityManagerInterface $entityManager, BasketRepository $basketRepository): Response
     {
+        $user = $this->getUser();
+        $baskets = $basketRepository->findBy(['user' => $user]);
+    
+        $productsInBasket = [];
+        foreach ($baskets as $basket) {
+            $productsInBasket = array_merge($productsInBasket, $basket->getProduct()->toArray());
+        }
+   
         return $this->render('basket/index.html.twig', [
-            'controller_name' => 'BasketController',
+            'productsInBasket' => $productsInBasket,
         ]);
+    }
+
+    #[Route('/basket/delete/{productId}', name: 'app_delete_basket')]
+    public function deleteBasketItem(EntityManagerInterface $entityManager, ProductRepository $productRepository, BasketRepository $basketRepository, int $productId): Response
+    {
+        $user = $this->getUser();
+        $product = $productRepository->find($productId);
+    
+        if (!$product) {
+            throw $this->createNotFoundException('Produit non trouvé');
+        }
+    
+        $baskets = $basketRepository->findBy(['user' => $user]);
+    
+        foreach ($baskets as $basket) {
+            $basket->removeProduct($product);
+        }
+    
+        $entityManager->flush();
+    
+        return $this->redirectToRoute('app_basket');
     }
 }
--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Basket;
+use App\Entity\Product;
 use App\Repository\UserRepository;
 use App\Repository\ProductRepository;
 use Doctrine\ORM\EntityManagerInterface;
@@ -21,6 +23,25 @@ class HomeController extends AbstractController
         return $this->render('home/index.html.twig', [
             'user' => $user,
             'products' => $products,
+        ]);
+    }
+
+    #[Route('/home/addToCard/{id}', name: 'app_addToCart')]
+    public function addToCart(EntityManagerInterface $entityManager, Request $request, ProductRepository $productRepository, int $id): Response
+    {
+        $user = $this->getUser();
+        $basket = new Basket();
+        $product = $productRepository->find($id);
+    
+        $basket->addProduct($product);
+        $user->addProduct($basket);
+    
+        $entityManager->persist($basket);
+        $entityManager->flush();
+                return $this->redirectToRoute('app_home');
+        return $this->render('home/addToCart.html.twig', [
+        
+            
         ]);
     }
 }
